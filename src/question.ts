81329ac77be58ff1b5e63e272aa308e872242f8e
Amanuel Shiferaw <123aman64@gmail.com>
2022-12-05 15:09:31 +0300

    Remove Ignore question.ts from gitignore

---
+++ src/question.ts
@@ -0,0 +1,285 @@
+export const biology = [
+  {
+    questionText: 'Cells  were first seen using the microscope by:',
+    option_a: 'Schwann',
+    option_b: 'Leeuwenhoek',
+    option_c: 'Schleiden',
+    option_d: 'Robert Hooke',
+    answer: 'option_d',
+    description:
+      ' Robert Hooke is an English scientist who has first seen dead cork cells. He is also the first person to coin the name cell.',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText:
+      'Of the following organic molecules, which one includes a Pentose sugar?',
+    option_a: 'Fructose',
+    option_b: 'Galactose',
+    option_c: 'Glucose',
+    option_d: 'Deoxyribose',
+    answer: 'option_d',
+    description:
+      'Pentose sugar is a 5 - carbon sugar which contains deoxy ribose sugar.',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText:
+      'In the energy pyramid, which group of organisms has the least amount of energy?',
+    option_a: 'Tertiary produces',
+    option_b: 'Primary producers',
+    option_c: 'Animals',
+    option_d: 'Herbivores',
+    answer: 'option_a',
+    description:
+      'The energy content in a food chain decreases as trophic level increases.',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText: 'The ultimate source of genetic variation is?',
+    option_a: 'Migration',
+    option_b: 'Selection',
+    option_c: ' Genetic drift',
+    option_d: 'Mutation',
+    answer: 'option_d',
+    description:
+      'Mutation is a sudden change in a genetic make-up of an organism. It results in genetic variation after successive generation.',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText:
+      'Which of the following is the first step in starting a research process?',
+    option_a: 'Identification of problem',
+    option_b: 'Searching for solutions to the problem',
+    option_c: ' Searching sources of information to located problem.',
+    option_d: 'Survey of related literature',
+    answer: 'option_a',
+    description:
+      'Accepting or rejecting the hypothesi Drawing conclusion on a solution retadeol to a problem',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText: 'Which of the following is lacking in prokaryotic cells?',
+    option_a: 'Cellulose',
+    option_b: 'Cytosol',
+    option_c: 'Ribosome',
+    option_d: 'DNA strand',
+    answer: 'option_a',
+    description:
+      'cellulose is a component of plant cell wall and not prokaryotic organisms',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText:
+      'What kind of nutrition do most of the decomposers have in general?',
+    option_a: 'Endosymbiont nutrition',
+    option_b: 'Chemosynthetic nutrition',
+    option_c: 'Saprobiotic nutrition',
+    option_d: 'Autotrophic nutrition',
+    answer: 'option_b',
+    description:
+      'Chemosynthetic nutrition is decomposition of dead bodies of organisms carried out by bacterium and fungi',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText:
+      'The practice of modern agriculture that reduces crop biodiversity is:',
+    option_a: 'Overgrazıng',
+    option_b: 'Forest clearing',
+    option_c: 'Growing few high yielding varieties',
+    option_d: 'Growing landraces',
+    answer: 'option_c',
+    description:
+      'Growing only high yielding varieties of crops may reduce the biodiversity of an ecosystem.',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText:
+      'One of the following is NOT true about protein synthesis in eukaryotes.',
+    option_a: 'Transcription takes place in the nucleus',
+    option_b: 'Both transcription and translation take place in the cytoplasm',
+    option_c: 'mRNA is modified after transcription',
+    option_d: 'Translation takes place in the cytoplasm',
+    answer: 'option_d',
+    description: 'The process of translation takes place in the cytoplasm.',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+  {
+    questionText:
+      'How many molecules of glycerol and fatty acids, respectively, are needed to form 100 molecules of triglyceride fat?',
+    option_a: '100 and 300',
+    option_b: '50 and 50',
+    option_c: '100 and 100',
+    option_d: '150 and 400',
+    answer: 'option_c',
+    description:
+      ' 1 molecule of triglyceride has 3 fatty acid and 1 glycerol molecules. Thus, 100 molecules of triglyceride has 100',
+    image: 'img/some-image.jpg',
+    course: 1,
+    year: '2014',
+  },
+];
+export const physics = [
+  {
+    questionText:
+      'Which of the following statement is correct about errors in measurement?',
+    option_a: 'Error make measured values same as accepted instruments.',
+    option_b: 'Error occur in every scientific investigation.',
+    option_c: 'Experimental error is the same as a mistake.',
+    option_d: 'Error can be avoided by using modern measuring instruments.',
+    answer: 'option_d',
+    description:
+      ' During measurement errors can be avoided by using modern measuring instruments',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+  {
+    questionText:
+      'What can be concluded when the number of significant figures increase?',
+    option_a: 'Accuracy increases',
+    option_b: 'Uncertainty increases',
+    option_c: 'Precision increases',
+    option_d: 'True reading increases.',
+    answer: 'option_a',
+    description:
+      ' when the number of significant figures increases during measurement the measured value is very close to the actual value and the measurement is more accurate or accuracy increases',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+
+  {
+    questionText:
+      'Tow forces F1= (8i+8J) N and F2 = (4i+6J) N are acting on an object. What is the magnitude and direction of the resultant force?',
+    option_a: '5N,1430',
+    option_b: '5N,-370',
+    option_c: '225N,1430',
+    option_d: '15N,370',
+    answer: 'option_d',
+    description: ' The magnitude of the resultant force is  = 15N and 370',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+  {
+    questionText: 'Which of the following is not correct about magnetic field?',
+    option_a: 'Magnetic field is produced by moving charge',
+    option_b: 'Magnetic monopoles exist',
+    option_c: 'The closer the field lines are, the strong the magnetic field.',
+    option_d: 'Magnetic field lines are closed',
+    answer: 'option_d',
+    description:
+      ' magnetic field lines are closed lines without beginning and end.',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+  {
+    questionText:
+      'A car start moving from rest and its motion on a straight line is showing in the figure below. What is the average velocity of the car within the first 6 seconds?',
+    option_a: 'C. 5 m/s',
+    option_b: 'D. 6 m /st',
+    option_c: 'A. 1.67 m/ s',
+    option_d: 'B. 5.67 m/s',
+    answer: 'option_d',
+    description:
+      'The average speed of the car during this time is Vave = Stot/t = 34m/6sec = 5.67m/',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+  {
+    questionText:
+      'Which one of the following statements is correct regarding the motion a plane?',
+    option_a:
+      'The centripetal force for a body moving in either vertical or horizontal circle is toward the center.',
+    option_b:
+      'In projectile motion the horizontal component of the motion is uniformly accelerated motion.',
+    option_c: 'When a body moves in a vertical circle, its speed is constant.',
+    option_d:
+      'When a body moves in a horizontal circle, its velocity is constant.',
+    answer: 'option_a',
+    description:
+      'the centripetal force for a body moving in either vertical or horizontal circle is directed toward the center.',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+  {
+    questionText:
+      ' A bullet is fired at an angle of 300 above the horizontal. If air resistance is neglected, what should be the bullet’s horizontal accelerated (ax) and vertical acceleration (ay) when it reach its maximum height?',
+    option_a: 'Both ax and ay are zero.',
+    option_b: 'Both ax and ay are 10 m/s2 downward',
+    option_c: 'ax is 10 m/s downward, and ay is zero.',
+    option_d: 'a x is zero and a y, is 10 m/s2 downward.',
+    answer: 'option_d',
+    description:
+      'as the bullet reaches the max height (point A) its acceleration in the Y-axis is 10m/s2 in magnitude and direction down ward. Since the horizontal motion is uniform through ax = 0.',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+  {
+    questionText:
+      'Two students A and B, with masses mA and mg respectively, stand on a wheeled trolleys as shown in the figure below. If students A has larger mass and pushes student B, then what can be said about their accelerations?',
+    option_a: 'The acceleration of student B is less than that of student A.',
+    option_b: 'The acceleration of student A is less than that of student B.',
+    option_c:
+      'There would be no acceleration because the force are equal and opposite.',
+    option_d:
+      'Both boys will be accelerated by the same magnitude bat in opposite directions.',
+    answer: 'option_b',
+    description:
+      'when student A pushes student B by exerting a force F to the right student B reacts by the same force in the opposite direction according to the statement of Newton’s third law of motion. Since they exert the same force on each other with mA > mB student B will have more acceleration after impact.',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+  {
+    questionText:
+      'For a constant force F, if the impulse imparted to the system is large, then what can conclude about the collision??',
+    option_a: 'Kinetic energy is conserved.',
+    option_b: 'The change in velocity is small.',
+    option_c: 'The contact or impact time is large.',
+    option_d: 'Linear momentum is conserved.',
+    answer: 'option_d',
+    description:
+      'the impulse imparted by a Constance force F on is given by J = ∆p = F. ∆t . The impulse imparted is large when the contact or impact time is large because F is constant.',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+  {
+    questionText:
+      ' An open trolley is rolling on a level surface without friction loss through a vertical downpour of rain, as shown in the in the figure belowAs the trolley rolls, an appreciable amount of rainwater accumulates in the cart. What will be the speed of the cart?',
+    option_a: 'It decreases because of conservation of momentum.',
+    option_b: 'It increases because of conservation of mechanical energy.',
+    option_c: 'It decrease because of conservation of mechanical energy.',
+    option_d:
+      'It remain the same because the raindrop are falling perpendicular to the direction of the cart’s motion.',
+    answer: 'option_a',
+    description:
+      'the collision between the open trolley of mass m1 and the mass of a vertical downward of rain m2 is in elastic momentum is conserved..',
+    image: 'img/some-image.jpg',
+    course: 3,
+    year: '2014',
+  },
+];
