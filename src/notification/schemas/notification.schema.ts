9592d339ecc5def79be72db8e969158002d4a027
Amanuel Shiferaw <123aman64@gmail.com>
2023-08-05 16:54:19 +0300

    refactor: add reference to users id on notification schema

--- src/notification/schemas/notification.schema.ts
+++ src/notification/schemas/notification.schema.ts
@@ -20,13 +20,17 @@ export class Notification {
   @Prop({ default: false })
   isLink: boolean;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
   userId: mongoose.Schema.Types.ObjectId; // to which users it belongs
 
   @Prop({ type: mongoose.Schema.Types.ObjectId })
   referenceId: mongoose.Schema.Types.ObjectId; // it could refer challenges or messages
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, isRequired: false }) // opponentUser
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    isRequired: false,
+    ref: 'User',
+  }) // opponentUser
   opponentUser: mongoose.Schema.Types.ObjectId | null | undefined;
 }
 
